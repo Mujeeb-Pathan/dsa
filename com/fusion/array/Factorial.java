da74f5613a883034f68d4ce924de902fc411cdbe
Mujeeb Khan <mujeeb.khan@viacomcontractor.com>
2026-03-06 00:02:53 +0530

    Added maven and factorial impl

---
+++ com/fusion/array/Factorial.java
@@ -0,0 +1,6 @@
+public class Factorial {
+
+	public static void main() {
+		System.out.println();
+	}
+}
---
+++ dsa/src/main/java/com/fusion/dsa/Factorial.java
@@ -0,0 +1,15 @@
+package com.fusion.dsa;
+
+public class Factorial {
+
+	public static int factorial(int n) {
+		if (n == 0 || n == 1) {
+			return 1;
+		}
+		return n * factorial(n - 1);
+	}
+
+	public static void main() {
+		System.out.println(factorial(5)); // Output: 120
+	}
+}
