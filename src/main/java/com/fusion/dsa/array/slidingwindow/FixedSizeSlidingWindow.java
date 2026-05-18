8e18ac8130ad7063d35b4601f87237685f45e2ea
Mujeeb Khan <mujeebkhanka@gmail.com>
2026-05-19 00:46:02 +0530

    Solved fixed size sliding window problem

---
+++ src/main/java/com/fusion/dsa/array/slidingwindow/FixedSizeSlidingWindow.java
@@ -0,0 +1,28 @@
+package com.fusion.dsa.array.slidingwindow;
+
+public class FixedSizeSlidingWindow {
+
+    public static int maxSum(int[] arr, int k) {
+        int windowSum = 0;
+        int maxSum = 0;
+
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+        maxSum = windowSum;
+
+        for (int i = k; i < arr.length; i++) {
+            windowSum = arr[i] + windowSum - arr[i - k];
+            maxSum= Math.max(maxSum, windowSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main() {
+        int[] arr = {2,  1,  5,  1,  3,  2};
+
+        System.out.println(maxSum(arr, 3));
+    }
+
+}
